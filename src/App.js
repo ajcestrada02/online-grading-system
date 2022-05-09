8167106d5e17959d468f50a186788808704b9522
Ace Estrada <Amelito.C.EstradaJr@pccw.com>
2022-05-09 21:45:42 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect, Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
@@ -7,23 +7,32 @@ import Admin from './pages/admin/Admin';
 import Login from './pages/login/Login';
 import Registration from './pages/registration/Registration';
 
-function App() {
+class App extends Component {
   // TODO: Need to have a login function
   // const [token, setToken] = useState();
 
   // if(!token){
     // return <Login setToken={setToken}/>
   // }
-  return (
-    <Router>
-        <Routes>
-          <Route exact path="/" element={<Landing/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/registration" element={<Registration/>}/>
-          <Route path="/admin" element={<Admin/>}/>
-        </Routes>
-    </Router>
-  );
+
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+  render(){
+    return(
+      <Router>
+          <Routes>
+            <Route exact path="/" element={<Landing/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/registration" element={<Registration/>}/>
+            <Route path="/admin" element={<Admin/>}/>
+          </Routes>
+      </Router>
+    )
+  }
 }
 
 export default App;
